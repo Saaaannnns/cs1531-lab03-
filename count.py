6851590e93870e15cb3e431f6b676dc1f469986c
Saaaannnns <github_z5174755@student.unsw.edu.au>
2018-08-04 14:42:45 +1000

    complete except bonus

--- count.py
+++ count.py
@@ -11,21 +11,58 @@
 def count_char(text):
     pass
     # add your code here
+    count = {}
+    for char in text:
+        count[char] = text.count(char)
+    
+    for char in count:
+        print(char ,count[char])
+
 
 def count_char_insensitive(text):
     pass
     # add your code here
+    count = {}
+    lower = text.lower()
+    for char in lower: 
+        count[char] = lower.count(char)
+    
+    for char in count:
+        print(char,count[char])        
+
 
 # bonus task:
 def count_char_ordered(text):
     pass
-    # add your code here 
+    # add your code here
+    count = {}
+    lower = text.lower()
+    start = lower[0]
+    for char in lower:
+        count[char] = lower.count(char)
+    
+    largest = count[start]
+    while len(count) > 0:
+        str = ''
+        for char in count:
+            if count[char] >= largest:
+                largest = count[char]
+                target = char
+        print(target,largest)
+        del count[target]
+        
+        for char in count:
+            str += char
+        
+        if len(count) > 0:
+            largest = count[str[0]]
+    
 
 text1 = 'I felt happy because I saw the others were happy and because I knew I should feel happy but I wasn’t really happy' # Robert Bolano
-text2 = 'HellO, WorLd!'
+text2 = 'HellO,WorLd!'
 
 # testing exercise 2
-count_char(text2)
-count_char_insensitive(text2)
+#count_char(text2)
+#count_char_insensitive(text2)
 count_char_ordered(text2)
 
